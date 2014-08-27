c0cb2d3010731b24c682de853e2f3914565b4240
salember <salember@users.noreply.github.com>
2014-08-27 19:43:13 -0400

    Update DatabaseContract.java

--- DatabaseContract.java
+++ DatabaseContract.java
@@ -9,14 +9,14 @@ import android.provider.BaseColumns;
 
 public final class DatabaseContract {
 
-    public static final  int    DATABASE_VERSION   = 1;
-    public static final  String DATABASE_NAME      = "Coin.db";
-    public static final  String UNIQUE_ID		   = "_id";
-    private static final String STEXT_TYPE          = " VARCHAR(15)";
-    private static final String MTEXT_TYPE          = " VARCHAR(100)";
-    private static final String DEC_TYPE          = " DECIMAL";
-    private static final String DATE_TYPE         = " DATETIME";
-    private static final String COMMA_SEP          = ",";
+    public static final  int    DATABASE_VERSION    = 1;
+    public static final  String DATABASE_NAME       = "Coin.db";
+    public static final  String UNIQUE_ID   = "_id";
+    private static final String STEXT_TYPE  = " VARCHAR(15)";
+    private static final String MTEXT_TYPE  = " VARCHAR(100)";
+    private static final String DEC_TYPE    = " DECIMAL";
+    private static final String DATE_TYPE   = " DATETIME";
+    private static final String COMMA_SEP   = ",";
   
 
     // To prevent someone from accidentally instantiating the contract class,
@@ -24,7 +24,7 @@ public final class DatabaseContract {
     public DatabaseContract() {}
 
     public static abstract class TableCoin implements BaseColumns {
-        public static final String TABLE_NAME       = "coin";
+        public static final String TABLE_NAME = "coin";
         public static final String COL1 = "name";
         public static final String COL2 = "ticker";
         public static final String COL3 = "currency";
@@ -47,7 +47,7 @@ public final class DatabaseContract {
     }
     
     public static abstract class TableValues implements BaseColumns {
-        public static final String TABLE_NAME       = "coinValues";
+        public static final String TABLE_NAME  = "coinValues";
         public static final String COL1 = "coin_id";
         public static final String COL2 = "source";
         public static final String COL3 = "api";
